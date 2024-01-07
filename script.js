9ba33882a80742e0bfb22c3f1a00d98abef5202b
late-bloomer82 <nateriver72@outlook.com>
2024-01-07 16:00:49 -0500

    built a basic form using the dom

--- script.js
+++ script.js
@@ -14,9 +14,12 @@ let button = document.querySelector('button');
 
 
 
+function form(){
+
 
 //Create form
 let form = document.createElement('form');
+
 
 //Create all 4 labels(input fields)
 
@@ -28,7 +31,8 @@ let authorInput = document.createElement('input');
 authorInput.type = 'text';
 authorInput.name = 'author';
 authorInput.placeholder = 'Enter author name...';
-
+form.appendChild(authorLabel);
+form.appendChild(authorInput);
 
 // #2
 let titleLabel = document.createElement('label');
@@ -38,6 +42,8 @@ let titleInput = document.createElement('input');
 titleInput.type = 'text';
 titleInput.name = 'title';
 titleInput.placeholder = 'Enter book title...';
+form.appendChild(titleLabel);
+form.appendChild(titleInput);
 
 
 // #3
@@ -48,6 +54,8 @@ let numberOfPagesInput = document.createElement('input');
 numberOfPagesInput.type = 'number';
 numberOfPagesInput.name = 'numberOfPagesInput';
 numberOfPagesInput.placeholder = 'Enter number of pages...';
+form.appendChild(numberOfPagesLabel);
+form.appendChild(numberOfPagesInput);
 
 // #4
 let readOrNotLabel = document.createElement('label');
@@ -56,6 +64,17 @@ readOrNotLabel.textContent = 'Have you read this book?';
 let readOrNotInput = document.createElement('input');
 readOrNotInput.type = 'checkbox';
 readOrNotInput.name = 'readOrNotInput';
+form.appendChild(readOrNotLabel);
+form.appendChild(readOrNotInput);
+
+let submitButton = document.createElement('button');
+submitButton.type = 'submit';
+submitButton.textContent = 'Submit';
+submitButton.onclick = addBookToLibrary();
+form.appendChild(submitButton);
+
+document.body.appendChild(form);
+}
 
 
 
@@ -67,10 +86,7 @@ readOrNotInput.name = 'readOrNotInput';
 
 
 
-
-button.addEventListener('click', ()=>{
-    
-})
+button.addEventListener('click', form);
 
 
 
